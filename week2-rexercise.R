e82c50aafd66ab0051e4de3b025fc5a07cf822f7
Kristian Krankenbuhl <chkr0001@students.zhaw.ch>
2021-05-06 19:57:45 +0200

    starting task 4

--- week2-rexercise.R
+++ week2-rexercise.R
@@ -43,3 +43,45 @@ caro <- st_as_sf(caro, coords = c("E", "N"), crs = 2056,
 caro_3 <- slice(caro, seq(1, 200, 3))
 caro_6 <- slice(caro, seq(1, 200, 6))
 caro_9 <- slice(caro, seq(1, 200, 9))
+
+#calculating speed, timelag and steplength for caro_3,6 and 9
+#caro_3
+timelag_3 <- mean(
+  na.omit(
+    as.integer(difftime(caro_3$DatetimeUTC, 
+                        lag(caro_3$DatetimeUTC),
+                        units = "secs"))
+  ))
+
+Ediff_3 <- mean(na.omit(lead(caro_3$E) - caro_3$E))
+Ndiff_3 <- mean(na.omit(lead(caro_3$N) - caro_3$N))
+steplength_3 <- sqrt(Ediff_3^2+Ndiff_3^2)
+
+speed_3 <- steplength_3 / timelag_3
+#caro_6
+timelag_6 <- mean(
+  na.omit(
+    as.integer(difftime(caro_6$DatetimeUTC, 
+                        lag(caro_6$DatetimeUTC),
+                        units = "secs"))
+  ))
+
+Ediff_6 <- mean(na.omit(lead(caro_6$E) - caro_6$E))
+Ndiff_6 <- mean(na.omit(lead(caro_6$N) - caro_6$N))
+steplength_6 <- sqrt(Ediff_6^2+Ndiff_6^2)
+
+speed_6 <- steplength_6 / timelag_6
+
+#caro_9
+timelag_9 <- mean(
+  na.omit(
+    as.integer(difftime(caro_9$DatetimeUTC, 
+                        lag(caro_9$DatetimeUTC),
+                        units = "secs"))
+  ))
+
+Ediff_9 <- mean(na.omit(lead(caro_9$E) - caro_9$E))
+Ndiff_9 <- mean(na.omit(lead(caro_9$N) - caro_9$N))
+steplength_9 <- sqrt(Ediff_9^2+Ndiff_9^2)
+
+speed_9 <- steplength_9 / timelag_9 
