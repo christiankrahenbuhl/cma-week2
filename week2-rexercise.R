b3c7b81fd6a03a0a7104b404f58c30e35c481821
Kristian Krankenbuhl <chkr0001@students.zhaw.ch>
2021-05-06 19:18:26 +0200

    task 2 done

--- week2-rexercise.R
+++ week2-rexercise.R
@@ -8,10 +8,13 @@ library(lubridate)    # To handle dates and times
 wildschwein_BE <- read_delim("wildschwein_BE_2056.csv",",")
 wildschwein_BE <- st_as_sf(wildschwein_BE, coords = c("E", "N"), 
                            crs = 2056, remove = FALSE)
-#calculating time diff betwen 2 and 1, then 3 and 2 etc...
-timelag <- as.integer(difftime(wildschwein_BE$DatetimeUTC, 
+#mean of time differences
+timelag <- mean(
+  na.omit(
+  as.integer(difftime(wildschwein_BE$DatetimeUTC, 
                                lag(wildschwein_BE$DatetimeUTC),
                                units = "secs"))
+                                              ))
 
 #How many individuals were tracked?
 wildschwein_BE$TierName %>% as.factor %>% summary (maxsum=60000) %>% 
@@ -29,5 +32,7 @@ wildschwein %>%
 Ediff <- mean(na.omit(lead(wildschwein_BE$E) - wildschwein_BE$E))
 Ndiff <- mean(na.omit(lead(wildschwein_BE$N) - wildschwein_BE$N))
 steplength <- sqrt(Ediff^2+Ndiff^2)
+
+speed <- steplength / timelag
 
 #Task 3
