fcd5696a6332c7025bfe43fb14ac9dfbaef679db
Kristian Krankenbuhl <chkr0001@students.zhaw.ch>
2021-05-06 09:33:17 +0200

    task 1 - in progress

--- week2-rexercise.R
+++ week2-rexercise.R
@@ -6,8 +6,15 @@ library(terra)        # To handle raster data
 library(lubridate)    # To handle dates and times
 
 wildschwein_BE <- read_delim("wildschwein_BE_2056.csv",",")
-wildschwein_BE <- st_as_sf(wildschwein_BE, coords = c("E", "N"), crs = 2056, remove = FALSE)
+wildschwein_BE <- st_as_sf(wildschwein_BE, coords = c("E", "N"), 
+                           crs = 2056, remove = FALSE)
 #calculating time diff betwen 2 and 1, then 3 and 2 etc...
-timediff <- difftime(wildschwein_BE$DatetimeUTC[2:51246],  wildschwein_BE$DatetimeUTC[1:51245],units = "secs")
-#transforming time diff into integer
-timediff <- as.integer(timediff)
+timelag <- as.integer(difftime(wildschwein_BE$DatetimeUTC, 
+                lag(wildschwein_BE$DatetimeUTC),
+                  units = "secs"))
+
+#How many individuals were tracked?
+wildschwein_BE$TierName %>% as.factor %>% summary (maxsum=60000) %>% 
+  length
+
+
